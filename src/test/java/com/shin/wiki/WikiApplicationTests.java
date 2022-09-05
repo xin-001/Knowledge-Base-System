eaee4c42cd1ad7a92e3ba42aa5e5c1b9a4eab78c
Xin Ma <xinma.mba@outlook.com>
2022-09-05 14:26:20 -0500

    2-2 init

---
+++ src/test/java/com/shin/wiki/WikiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.shin.wiki;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WikiApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
