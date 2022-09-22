4d2c978b1835d6777a403382a1af53ad66446cb5
Xin Ma <xinma.mba@outlook.com>
2022-09-22 16:43:35 -0500

    3-3 add all.sql/second time/

---
+++ src/main/java/com/shin/wiki/mapper/TestMapper.java
@@ -0,0 +1,9 @@
+package com.shin.wiki.mapper;
+
+import com.shin.wiki.domain.Test;
+
+import java.util.List;
+
+public interface TestMapper {
+    public List<Test> list();
+}
---
+++ src/main/java/com/shin/wiki/service/TestService.java
@@ -0,0 +1,19 @@
+package com.shin.wiki.service;
+
+import com.shin.wiki.domain.Test;
+import com.shin.wiki.mapper.TestMapper;
+import org.springframework.stereotype.Service;
+
+import javax.annotation.Resource;
+import java.util.List;
+
+@Service
+public class TestService {
+
+    @Resource
+    private TestMapper testMapper;
+
+    public List<Test> list(){
+        return testMapper.list();
+    }
+}
